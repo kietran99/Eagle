0137c182f6ad40407787193eca86cf023b8995af
kietran99 <kietran99@gmail.com>
2024-06-22 15:34:44 +0700

    Add Premake scripts

---
+++ App/Build-App.lua
@@ -0,0 +1,46 @@
+project "App"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+
+    files
+    {
+        "Source/**.h",
+        "Source/**.cpp",
+    }
+
+    includedirs
+    {
+        "Source",
+        "../Core/Source",
+    }
+
+    links
+    {
+        "Eagle",
+    }
+
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+    filter "system:windows"
+        systemversion "latest"
+        defines {}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "On"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines { "NDEBUG" }
+        runtime "Release"
+        optimize "On"
+        symbols "Off"
---
+++ Build.lua
@@ -0,0 +1,15 @@
+workspace "Eagle-Sandbox"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist"}
+    startproject "App"
+
+    filter "system:windows"
+        buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Core"
+    include "Core/Build-Core.lua"
+group ""
+
+include "App/Build-App.lua"
